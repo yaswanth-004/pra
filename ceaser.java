6fb76efaaf11823328ee3bca52c039874034c388
Mallidi Yaswanth <145952092+yaswanth-004@users.noreply.github.com>
2025-11-15 08:48:12 +0530

    java

---
+++ ceaser.java
@@ -0,0 +1,34 @@
+public class CaesarCipher {
+
+    public static String encode(String enc, int offset) {
+        offset = offset % 26 + 26;
+        StringBuilder encoded = new StringBuilder();
+        for (char c : enc.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if (Character.isUpperCase(c)) {
+                    encoded.append((char) ('A' + (c - 'A' + offset) % 26));
+                } else {
+                    encoded.append((char) ('a' + (c - 'a' + offset) % 26));
+                }
+            } else {
+                encoded.append(c);
+            }
+        }
+        return encoded.toString();
+    }
+
+    public static String decode(String enc, int offset) {
+        return encode(enc, 26 - offset);
+    }
+
+    public static void main(String[] args) throws Exception {
+        String msg = "Anna University";
+        System.out.println("Simulating Caesar Cipher");
+        System.out.println("------------------------");
+        System.out.println("Input : " + msg);
+        System.out.print("Encrypted Message : ");
+        System.out.println(CaesarCipher.encode(msg, 3));
+        System.out.print("Decrypted Message : ");
+        System.out.println(CaesarCipher.decode(CaesarCipher.encode(msg, 3), 3));
+    }
+}
